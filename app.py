eac23bb179a3f3f70418363ed5d92d0a7b83c1e2
Nishant Sahoo <nishantsahoo.in@gmail.com>
2019-01-05 01:02:13 +0530

    Update

--- app.py
+++ app.py
@@ -1,8 +1,17 @@
 from flask import Flask
+import smtplib
 app = Flask(__name__)
 
 @app.route('/')
 def homepage():
+	server = smtplib.SMTP('smtp.gmail.com', 587)
+	#Next, log in to the server
+	server.starttls()
+	server.login("", "")
+
+	#Send the mail
+	msg = "Yo"
+	server.sendmail("", "", msg)
     return("Hello!")
 
 if __name__ == '__main__':
