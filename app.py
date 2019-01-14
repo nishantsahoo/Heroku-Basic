77b0eb05e80f757631d441da6183f185bacf7274
Nishant Sahoo <nishantsahoo.in@gmail.com>
2019-01-14 23:03:42 +0530

    Update

--- app.py
+++ app.py
@@ -14,5 +14,20 @@ def homepage():
 	server.sendmail("", "", msg)
     return("Hello!")
 
+@app.route('/', methods=['POST'])
+def sendmail():
+	if not request.form['body']:
+		return "Please enter some data"
+
+	server = smtplib.SMTP('smtp.gmail.com', 587)
+	#Next, log in to the server
+	server.starttls()
+	server.login("", "")
+
+	# Send the mail
+	msg = request.form['body']
+	server.sendmail("", "", msg)
+	return("Mail sent with post message!")
+
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
